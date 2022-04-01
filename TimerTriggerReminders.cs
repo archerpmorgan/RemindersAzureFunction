b3d7f4b1c1e58ba0c31e50dfbaf580246d1efc58
archerpmorgan <armorgan@microsoft.com>
2022-04-01 10:34:00 -0700

    basic implementation

--- TimerTriggerReminders.cs
+++ TimerTriggerReminders.cs
@@ -2,15 +2,28 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
 namespace TimerTriggerReminders.Function
 {
     public class TimerTriggerReminders
     {
         [FunctionName("TimerTriggerReminders")]
-        public void Run([TimerTrigger("*/1 * * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("*/1 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var personalEmail = Environment.GetEnvironmentVariable("PERSONAL_EMAIL");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(personalEmail, "Archer");
+            var subject = "Test email";
+            var to = new EmailAddress(personalEmail, "Archer");
+            var plainTextContent = "Test content";
+            var htmlContent = "<strong>Test content</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
--- spec.md
+++ spec.md
@@ -3,11 +3,13 @@
 1. codebase in github
 2. timer azure function runs every minute
 3. random number in function dictates that only every so often the action actually does anything
-4. sends and SMS to my phone using Azure Communication Services
+    In the end I get about 2-3 reminders a day
+4. sends an email to my personal address using Sendgrid
 
 
 - storage and ZCS connection string in key vault
-- function app connects to key vault with managed identity
+- function app connects to key vault with managed identity to access sendgrid, azure storage, and personal email address
+<https://docs.microsoft.com/en-us/azure/cosmos-db/managed-identity-based-authentication>
 - database of reminders in storage account
 
 WBAUP73579VK76105
